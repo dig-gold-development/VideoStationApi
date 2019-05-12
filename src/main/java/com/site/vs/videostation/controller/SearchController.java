d582633e8c0c66f8327ccc24f597eca1591ac29e
zengfanjian <452275571@qq.com>
2019-05-12 19:59:48 +0800

    添加seach

---
+++ src/main/java/com/site/vs/videostation/controller/SearchController.java
@@ -0,0 +1,13 @@
+package com.site.vs.videostation.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "search")
+public class SearchController {
+
+
+}
